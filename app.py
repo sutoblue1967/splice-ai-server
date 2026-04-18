125284d03a42174df40f8d8585a460de275ca525
sutoblue1967 <shawn@splice.social>
2026-04-18 12:25:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import psycopg2
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 def load_events_from_file(filename: str) -> List[Dict[str, Any]]:
