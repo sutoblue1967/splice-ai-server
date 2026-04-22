0b83c2137521be5f9fa31561ee2b836e75c9edb7
sutoblue1967 <shawn@splice.social>
2026-04-22 13:48:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -890,8 +890,13 @@ def bulk_ingest_post():
                 "raw": line
             })
 
+       else:
+        # Detect address (very basic)
+        if any(word in line.lower() for word in ["street", "st", "avenue", "ave", "road", "rd", "parkersburg"]):
+        venue = line
         else:
             current_venue = line
+
 
 
     hidden_inputs = ""
